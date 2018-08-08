47388a0637fb253b53d6110b7993b625dbb98b7a
Xijian Yan <yanxijian@live.com>
2018-08-08 23:33:07 +0800

    Commit code

---
+++ common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define L_Menu_Text L"&Display File Name (C++)"
+
+#define Verb_Name "cppdisplay"
+#define L_Verb_Name L"cppdisplay"
+
+#define Verb_Canonical_Name "CppDisplayFileName"
+#define L_Verb_Canonical_Name L"CppDisplayFileName"
+
+#define Verb_Help_Text "Display File Name (C++)"
+#define L_Verb_Help_Text L"Display File Name (C++)"
+
+#define L_Friendly_Menu_Name L"CppShellExtContextMenuHandler.FileContextMenuExt"
+
+#define L_Friendly_Class_Name L"CppShellExtContextMenuHandler.FileContextMenuExt Class"
+
+#define L_Associated_Type L".cpp"
